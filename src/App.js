1ed8532ab4a8759dbb8063602f2f333528c297ce
Mekasu <mekasurenae@gmail.com>
2025-01-02 21:12:26 -0600

    incorporated authContext and Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/authContext';
 
 import Home from './pages/home';
+
+import Header from './components/header';
 
 export default function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthProvider>
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthProvider>
     </div>
-  )
-}
+  );
+};
